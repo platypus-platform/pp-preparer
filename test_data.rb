a34ec057fae35b68aed661dafba8ecdc2ac2b910
Xavier Shay <xavier@squareup.com>
2014-08-30 13:53:11 -0700

    Update test_data to be correct.

--- test_data.rb
+++ test_data.rb
@@ -17,14 +17,13 @@ version = {
 # Deploy config cannot be changed by developers. It requires priviliged access
 # to change, since it allows you to run stuff has root in arbitrary locations.
 deploy_config = {
-  'basedir' => '/tmp/slug',
-  'runas'   => nil
+  'basedir' => '/tmp/slug'
 }
 
-cmd = "curl -X PUT localhost:8500/v1/kv/clusters/#{node["cluster"]}/versions -d '#{version.to_json}'"
+cmd = "curl -X PUT localhost:8500/v1/kv/clusters/#{app}/#{node["cluster"]}/versions -d '#{version.to_json}'"
 puts cmd
 `#{cmd}`
 
-cmd = "curl -X PUT localhost:8500/v1/kv/clusters/#{node["cluster"]}/deploy_config -d '#{deploy_config.to_json}'"
+cmd = "curl -X PUT localhost:8500/v1/kv/clusters/#{app}/#{node["cluster"]}/deploy_config -d '#{deploy_config.to_json}'"
 puts cmd
 `#{cmd}`
